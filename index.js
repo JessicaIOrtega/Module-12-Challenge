50d93e3d4d8289f477d4a441351833ab2971bf0d
JessicaIOrtega <jes.ortega7@gmail.com>
2023-02-21 22:55:04 -0600

    added pseudo code to start functions for each choice given

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const inquirer = require('inquirer');
+const sql = require('./db/query');
 
-const questions = () => {
+
+
+// 'Add Department'
+newDept();
+
+
+//'Add Employee'
+newEmployee();
+
+
+//'Add Role'
+newRole();
+
+
+//'Update Employee Role'
+updateEmpRole();
+
+
+//'View all Departments'
+viewDepts();
+
+
+//'View all Employees'
+viewEmployees();
+
+
+//'View all Roles'
+viewRoles();
+
+
+const mainQuestions = () => {
     inquirer
         .prompt([
             {
@@ -28,6 +59,26 @@ const questions = () => {
             const { req } = data;
             console.log(req);
 
+            switch (req) {
+                case 'Add Department': newDept();
+                    break;
+                case 'Add Employee': newEmployee();
+                    break;
+                case 'Add Role': newRole();
+                    break;
+                case 'Update Employee Role': updateEmpRole();
+                    break;
+                case 'View all Departments': viewDepts();
+                    break;
+                case 'View all Employees': viewEmployees();
+                    break;
+                case 'View all Roles': viewRoles();
+                    break;
 
+                default:
+                    break;
+            }
         })
-}
+};
+
+mainQuestions();
